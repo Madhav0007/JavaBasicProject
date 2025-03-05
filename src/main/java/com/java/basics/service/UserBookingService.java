da69e157ada8e997d20e6387bea469c674e3afe7
madhav <madhav.mehta@artemhealthtech.com>
2025-03-05 15:31:19 +0530

    Second commit

--- src/main/java/com/java/basics/service/UserBookingService.java
+++ src/main/java/com/java/basics/service/UserBookingService.java
@@ -3,13 +3,15 @@ package com.java.basics.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.java.basics.entity.User;
+import com.java.basics.util.UserServiceUtil;
 
 import java.io.IOException;
 import java.util.*;
 import java.io.File;
 
 public class UserBookingService {
-
+    //JSON->OBJECT --> DESERIALIZATION
+//OBJECT->JSON --> SERIALIZATION
     private User user;
     private List<User> userList;
     private ObjectMapper objectMapper = new ObjectMapper();
@@ -21,4 +23,33 @@ public class UserBookingService {
         userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
         });
     }
+
+    public Boolean loginUser() {
+        Optional<User> foundUser = userList.stream().filter(user1 ->
+        {
+            return user1.getName().equalsIgnoreCase(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1) {
+        try {
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        } catch (IOException e) {
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException {
+        File userFile = new File(USERS_PATH);
+        objectMapper.writeValue(userFile, userList);
+    }
+
+    public void fetchBooking()
+    {
+        user.printTickets();
+    }
+
 }
