822fa2ca51d17a2833a746d43d05ce4a2f121d86
madhav <madhav.mehta@artemhealthtech.com>
2025-03-05 12:05:43 +0530

    first commit

---
+++ src/main/java/com/java/basics/entity/User.java
@@ -0,0 +1,11 @@
+package com.java.basics.entity;
+
+import java.util.*;
+
+public class User {
+    private String name;
+    private String password;
+    private String hashedPassword;
+    private List<Ticket> ticketsBooked;
+    private String userId;
+}
